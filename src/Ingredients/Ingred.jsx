5309c66725f632063fd770ab1fbadd21f9ecd183
Sharath76744 <sharathkumars2005@gmail.com>
2026-06-24 22:45:04 +0530

    Changed the Ingredient's image

--- src/Ingredients/Ingred.jsx
+++ src/Ingredients/Ingred.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import IngredImg from "../assets/imgs/bf2afa6e-ab0f-417f-b75f-2419a6be6971.png";
 
 const Ingred = () => {
   return (
     <div className="ingred-container">
       <div className="ingred-cards">
         <div className="ingred-img">
-          <div className="img"></div>
+          <div
+            className="img"
+            style={{ backgroundImage: `url(${IngredImg})` }}
+          ></div>
         </div>
 
         <div className="ingredients">
